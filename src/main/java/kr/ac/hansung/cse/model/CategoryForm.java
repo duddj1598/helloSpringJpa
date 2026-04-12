ac33ba0e1ab1e3b9c6e2517c35d71207240140ff
juno <hellojuno6649@gmail.com>
2026-04-12 18:08:12 +0900

    feat: hint처리 (with Gemini)

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,16 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name;
+}
